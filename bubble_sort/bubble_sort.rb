bea0691f54b5d03f8ee0cb86cbecb1cb050b34d0
Matt Kragen <mattdkragen@gmail.com>
2021-12-23 17:53:42 -0800

    Create starter for Ruby bubble_sort

---
+++ bubble_sort/bubble_sort.rb
@@ -0,0 +1,12 @@
+# Bubble Sort
+
+def bubble_sort(array)
+  
+end
+
+arr1 = [4, 1, 7, 9, 3, 2, 6]
+arr2 = [8. 7, 6, 5, 4, 3, 2]
+arr3 = [-5, -8, 3, 2, -4, -3, 6]
+p buuble_sort(arr1)
+p buuble_sort(arr2)
+p buuble_sort(arr3)
