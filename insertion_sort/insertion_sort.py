867b5b36d34cc7c85974a5c96d7bf31daa53a85c
Matt Kragen <mattdkragen@gmail.com>
2021-12-30 13:12:25 -0800

    Remove error leading to unsorted final element

--- insertion_sort/insertion_sort.py
+++ insertion_sort/insertion_sort.py
@@ -1,7 +1,7 @@
 # Insertion Sort
 
 def insertion_sort(list):
-  for current in range(1, (len(list)) - 1):
+  for current in range(1, len(list)):
     i = current
     while i > 0 and list[i] < list[i - 1]:
       list[i], list[i - 1] = list[i - 1], list[i]
